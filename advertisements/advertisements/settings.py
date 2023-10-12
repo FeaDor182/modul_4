4a657fcb22cd1a0af55bf23807e7d5fc546ad9aa
FeaDor182 <fedikber@gmail.com>
2023-10-12 14:55:38 +0300

    0.0.4

--- advertisements/advertisements/settings.py
+++ advertisements/advertisements/settings.py
@@ -77,9 +77,10 @@ WSGI_APPLICATION = 'advertisements.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': BASE_DIR / 'hom.postgresql',
     }
+
 }
 
 
